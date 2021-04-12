2c548b63f77973b7f06327d48aaf8aae0cd34e3e
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-12 17:33:17 +0530

    [Priyanshu]added start position of player

---
+++ StartPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame
+{
+    class StartPosition
+    {
+         
+        public static void Start()
+        {
+            int START_POSITION = 0;
+            Console.WriteLine(" Single player at start position = " + START_POSITION);
+        }
+    }
+}
