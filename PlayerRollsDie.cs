32c5366532c62bcad23e944c57527718219ead67
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-12 17:35:29 +0530

    [Priyanshu]modified player rolls a die

---
+++ PlayerRollsDie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame
+{
+    class PlayerRollsDie
+    {
+        
+        public static void DieRoll()
+        {
+            //int START_POSITION = 0;
+            Random random = new Random();
+            int dieValue = random.Next(0, 6);
+            Console.WriteLine(" Player die roll position = " + dieValue);
+        }
+    }
+}
+   
